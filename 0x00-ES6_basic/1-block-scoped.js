6db9b7d2b6ba0617ce953b4a9f456e56db8c0b5f
DMMutua <lengowriter99@gmail.com>
2023-04-19 11:45:21 +0300

    using `let` to avoid variable overwriting. adding npm execution reqs

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,10 @@
-/**
- * Uses `const` and `let` to prevent variable overwritting inside blocks.
- * @returns {array} of booleans of task and task2 output.
- */
 export default function taskBlock(trueOrFalse) {
 	let task = false;
 	let task2 = true;
 
 	if (trueOrFalse) {
-		task = true;
-		task2 = false;
+		task = false;
+		task2 = true;
 	}
 
 
