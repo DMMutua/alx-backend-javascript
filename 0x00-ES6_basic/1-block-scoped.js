9f929ffff5d3f8382703f30f5169a589a579f798
DMMutua <lengowriter99@gmail.com>
2023-04-19 11:19:06 +0300

    preventing variable overwriting by block-scoping: correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,13 +3,14 @@
  * @returns {array} of booleans of task and task2 output.
  */
 export default function taskBlock(trueOrFalse) {
-	const task = false;
+	let task = false;
 	let task2 = true;
 
 	if (trueOrFalse) {
-		const task = true;
+		task = true;
 		task2 = false;
 	}
 
-	return [task, task2];`
+
+	return [task, task2];
 }
