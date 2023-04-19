e53f16e0f7118d8658634921602dc6e9a812b2b3
DMMutua <lengowriter99@gmail.com>
2023-04-19 11:53:29 +0300

    block scoping variable declaration

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,14 @@
+/**
+ * Using `let` and `const` for block scopping
+ * @returns {array} of output for task and task2
+ */
 export default function taskBlock(trueOrFalse) {
 	let task = false;
-	let task2 = true;
+	const task2 = true;
 
 	if (trueOrFalse) {
 		task = false;
-		task2 = true;
+		// const var task2 cannot be re-assigned
 	}
 
 
