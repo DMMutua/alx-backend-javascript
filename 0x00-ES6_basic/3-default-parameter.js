cd0ee7d2053b1ce7b23c550c3f7a495b0f282335
DMMutua <lengowriter99@gmail.com>
2023-04-19 12:26:53 +0300

    default standard function params in JS ES6

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,7 @@
+/**
+ * Function that uses default parameters.
+ * @return {int} result of addition
+ */
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+	return initialNumber + expansion1989 + expansion2019;
+}
