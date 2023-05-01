6898599895c01de70b0b55df0cde2266235550f8
DMMutua <lengowriter99@gmail.com>
2023-05-01 12:41:31 +0300

    adding the `.finally` to handle console printing regardless

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,18 +1,19 @@
 /**
- * Handles a response from an API Promise by adding resolution and rejection handlers
- * @param {Promise} promise - The Promise to handle
- * @returns {Object} - An object with status and body attributes on resolution, or an empty Error object on rejection
+ * Handles the response from an API Promise.
+ * @param {Promise} promise - The Promise to handle.
+ * @returns {Promise} - A Promise that resolves with an object containing status and body attributes if the input Promise resolves, or rejects with an empty Error object if the input Promise rejects.
  */
-
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(result => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(error => {
-      console.error(error);
-      return new Error();
-    });
+  return promise.then((response) => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  })
+  .catch((error) => {
+    console.log('Got a response from the API');
+    return new Error();
+  })
+  .finally(() => {
+    console.log('Got a response from the API');
+  });
 }
 
